2616a3b095d59254e67797b76a44dcd0a0bedb05
BJZ <468504329@qq.com>
2021-09-28 13:48:14 +0800

    单链表的修改和删除

--- example/src/cn/xiaoyes/algorithm/linkedlist/SingleLikedListDemo.java
+++ example/src/cn/xiaoyes/algorithm/linkedlist/SingleLikedListDemo.java
@@ -12,38 +12,48 @@ public class SingleLikedListDemo {
         HeroNode node4 = new HeroNode(4, "林冲", "豹子头");
 
         SingleLinkedList singleLinkedList = new SingleLinkedList();
-        singleLinkedList.add(node1);
-        singleLinkedList.add(node2);
-        singleLinkedList.add(node3);
-        singleLinkedList.add(node4);
+//        singleLinkedList.add(node1);
+//        singleLinkedList.add(node2);
+//        singleLinkedList.add(node3);
+//        singleLinkedList.add(node4);
 
         // 加入按照编号的顺序
-//        singleLinkedList.addByOrder(node1);
-//        singleLinkedList.addByOrder(node4);
-//        singleLinkedList.addByOrder(node2);
-//        singleLinkedList.addByOrder(node3);
-//        singleLinkedList.addByOrder(node3);
+        singleLinkedList.addByOrder(node1);
+        singleLinkedList.addByOrder(node4);
+        singleLinkedList.addByOrder(node2);
+        singleLinkedList.addByOrder(node3);
+        singleLinkedList.addByOrder(node3);
 
+        // 测试修改节点的代码
+        singleLinkedList.list();
+        HeroNode newHeroNode = new HeroNode(2,"小鹿","玉麒麟");
+        singleLinkedList.update(newHeroNode);
+        System.out.println("修改后的链表");
+        singleLinkedList.list();
+
+        System.out.println("删除后的链表");
+        // 测试删除节点的代码
+        singleLinkedList.delete(3);
         singleLinkedList.list();
     }
 
     // 定义SingleLinkedList管理英雄
-    static class SingleLinkedList{
+    static class SingleLinkedList {
 
         // 初始化一个头节点 不存放具体得数据
-        private HeroNode head = new HeroNode(0,"","");
+        private HeroNode head = new HeroNode(0, "", "");
 
         // 添加节点到单向链表
         // 当不考虑编号得顺序时
         // 找到当前链表得最后节点
         // 将最后这个节点得next指向新得节点
-        public void add(HeroNode node){
+        public void add(HeroNode node) {
             // 因为head节点不能动，因此我们需要一个辅助遍历指针
             HeroNode temp = head;
             // 遍历链表 找到最后
-            while (true){
+            while (true) {
                 // 找到链表的最后
-                if (temp.next == null){
+                if (temp.next == null) {
                     break;
                 }
                 // 如果没有找到最后 就将temp后移
@@ -55,48 +65,104 @@ public class SingleLikedListDemo {
 
         // 第二种方式在添加英雄时，根据排名建英雄插入到指定位置
         // 如果有这个排名 则添加失败 并且给出提示
-        public void addByOrder(HeroNode node){
+        public void addByOrder(HeroNode node) {
             // 因为头节点不能动 因此我们仍然同一个辅助变量来帮助我们找到变量的位置
             // 因此我们找的temp时位于添加位置的前一个节点，否则插入不了
             HeroNode temp = head;
             // flag表示添加的编号是否存在 默认为false
             boolean flag = false;
-            while (true){
+            while (true) {
                 // 说明temp已经在链表的最后了
-                if (temp.next == null){
+                if (temp.next == null) {
                     break;
                 }
                 // 位置就找到了 就在temp的后面插入
-                if (temp.next.no > node.no){
+                if (temp.next.no > node.no) {
                     break;
-                }else if (temp.next.no == node.no){// 希望添加的node的编号已经存在
+                } else if (temp.next.no == node.no) {// 希望添加的node的编号已经存在
                     flag = true;
                     break;
                 }
                 temp = temp.next;// 后移 遍历当前的链表
             }
-            if (flag){
+            if (flag) {
                 // 不能添加 说明编号已经存在
                 System.out.println("准备插入的英雄的编号已经存在 不能加入: " + node.no);
-            }else {
+            } else {
                 // 插入到链表中
                 node.next = temp.next;
                 temp.next = node;
             }
         }
 
-        // 显示链表[遍历]
-        public void list(){
-            // 先判断链表是否为空
+        // 修改节点得信息，根据no编号来修改，即no编号不能修改
+        // 根据node得no来修改
+        public void update(HeroNode node) {
+            // 判断是否为空
             if (head.next == null){
+                System.out.println("链表为空");
+                return;
+            }
+            // 找到需要修改得节点，根据no编号来找
+            HeroNode temp = head.next;
+            boolean flag = false;
+            while (true){
+                if (temp == null){
+                    break;// 到链表的最后 跳出循环
+                }
+                if (temp.no == node.no){
+                    // 找到节点
+                    flag = true;
+                    break;
+                }
+                temp = temp.next;
+            }
+            // 根据flag判断是否找到要修改的节点
+            if (flag){
+                temp.name = node.name;
+                temp.nickname = node.nickname;
+            }else {
+                // 没有找到节点
+                System.out.println("没有找到编号" + node.no + "的节点");
+            }
+        }
+
+        // 删除节点
+        // head节点不能动 因此我们需要一个temp复制节点找到待删除节点的前一个节点
+        // 说明我们在比较时，是temp.next.no和需要删除的节点的no比较
+        public void delete(int no){
+            HeroNode temp = head;
+            boolean flag = false;// 标志是否找到
+            while (true){
+                if (temp.next == null){// 已经到链表的最后
+                    break;
+                }
+                if (temp.next.no == no){
+                    // 找到哦了待删除节点的前一个节点
+                    flag = true;
+                    break;
+                }
+                temp = temp.next;// temp后移 遍历
+            }
+            if (flag){
+                temp.next = temp.next.next;
+            }else {
+                System.out.println("要删除的节点" + no + "不存在");
+            }
+        }
+
+        // 显示链表[遍历]
+        public void list() {
+            // 先判断链表是否为空
+            if (head.next == null) {
                 System.out.println("链表为空");
                 return;
             }
             // 因为头节点不能动，因此我们需要一个辅助变量来遍历
             HeroNode temp = head.next;
-            while (true){
+            while (true) {
                 // 判断是否到最后了
-                if (temp == null){
+                if (temp == null) {
                     break;
                 }
                 // 输出节点的信息
@@ -108,7 +174,7 @@ public class SingleLikedListDemo {
     }
 
     // 每一个HeroNode对象就是一个节点
-    static class HeroNode{
+    static class HeroNode {
 
         private int no;
         private String name;

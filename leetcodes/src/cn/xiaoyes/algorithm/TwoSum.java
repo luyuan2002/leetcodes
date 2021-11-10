cf18ec25f5922d9dbcec3261a933f77e0ea63337
BJZ <468504329@qq.com>
2021-11-10 17:56:11 +0800

    两数之和

---
+++ leetcodes/src/cn/xiaoyes/algorithm/TwoSum.java
@@ -0,0 +1,41 @@
+package cn.xiaoyes.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * 1. 两数之和
+ * 给定一个整数数组 nums 和一个整数目标值 target，
+ * 请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {3,2,4};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+}
